5fe64015ff095f1c17a12ef70fc7bc637ec90f6c
henrique <marquesx19@gmail.com>
2024-12-20 15:01:01 -0300

    class 14 end

--- src/App.js
+++ src/App.js
@@ -3,40 +3,44 @@ import './App.css';
 import { Component } from 'react';
 
  class App extends Component{
-  constructor(props){
-    super(props);
-    this.handlePClicado = this.handlePClicado.bind(this);
-    this.state = {
-      name: 'Henrique Marques'
+    state = {
+      name: 'Henrique Marques',
+      counter: 0
     };
-  }
-  handlePClicado(){
+
+  handlePClicked = () =>{
     this.setState({name: 'Matheus Marques'})
   }
+
+  handleAClick = (event) =>{
+    event.preventDefault()
+    const { counter } = this.state;
+
+    this.setState({counter: counter + 1})
+  }
+
   render(){ 
-    const {name} = this.state;
+    const {name, counter } = this.state;
     return(
          <div className="App">
             <header className="App-header">
               <img src={logo} className="App-logo" alt="logo" />
-              <p onClick ={this.handlePClicado} >
-               {name}
+              <p onClick ={this.handlePClicked} >
+               {name} {counter}
              </p>
               <a
+              onClick={this.handleAClick}
                className="App-link"
                 href="https://reactjs.org"
                 target="_blank"
                 rel="noopener noreferrer"
-                >
-                Learn React
+                > 
+                Este é o Link
             </a>
             </header>
          </div>
         );
   }
 } 
-//function App() {
-//  
-//}
 
 export default App;
